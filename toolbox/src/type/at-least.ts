81919ba5096699203860c560ce90a865eda9ebe3
yair <yairopro@gmail.com>
2025-04-17 10:10:19 +0300

    atLeast<> remove nulls

--- toolbox/src/type/at-least.ts
+++ toolbox/src/type/at-least.ts
@@ -1,7 +1,9 @@
+import type { required } from "./required";
+
 /**
  * Mark fields as optionnal exept certain keys.
  * ```
  *  @see atMost
  */
-export type atLeast<target extends object, fields extends keyof target = never> = Partial<target> &
-	Pick<target, fields>;
+export type atLeast<target extends object, fields extends keyof target = never> = Omit<target, fields> &
+	required<Pick<target, fields>>;
