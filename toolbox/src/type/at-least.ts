fef864d542167cdc70fbd83c4e2a5571f0736028
yair <yairopro@gmail.com>
2025-05-04 23:06:00 +0300

    chore

--- toolbox/src/type/at-least.ts
+++ toolbox/src/type/at-least.ts
@@ -6,4 +6,4 @@ import type { required } from "./required";
  *  @see atMost
  */
 export type atLeast<target extends object, fields extends keyof target = never> = Omit<Partial<target>, fields> &
-	required<target, fields>;
+	required<Pick<target, fields>>;
--- web/src/geolocation/getGeolocation.ts
+++ web/src/geolocation/getGeolocation.ts
@@ -2,10 +2,15 @@ import { HOUR_MS, SECOND_MS } from "@coffee-break/toolbox/time/constants"
 
 export default async function getGeolocation(fresh?: boolean) {
 	return new Promise<GeolocationPosition>((resolve, reject) => {
-		navigator.geolocation.getCurrentPosition(resolve, reject, {
+		navigator.geolocation.getCurrentPosition(position => {
+			getGeolocation.last = position;
+			resolve(position);
+		}, reject, {
 			maximumAge: fresh ? 0 : HOUR_MS,
 			enableHighAccuracy: fresh,
 			timeout: 30 * SECOND_MS,
 		});
 	});
 }
+
+getGeolocation.last = undefined as GeolocationPosition | undefined;
