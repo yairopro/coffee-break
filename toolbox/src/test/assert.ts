030eed17d67ead03c63bb21c506192f7404d41ce
yair <yairopro@gmail.com>
2025-06-04 08:30:55 +0300

    assert(who, ...)

--- toolbox/src/test/assert.ts
+++ toolbox/src/test/assert.ts
@@ -1,8 +1,18 @@
 import { is } from "ramda";
 
-export default function assert(mustBeTruthy: unknown, errorParam: unknown): asserts mustBeTruthy is {} {
+export default function assert(who: string, mustBeTruthy: unknown, errorParam: unknown): asserts mustBeTruthy is {} {
 	if (!mustBeTruthy) {
-		const error = is(String, errorParam) ? new Error(errorParam) : errorParam;
+		const error = is(String, errorParam) ? new Error(errorParam)
+			: errorParam instanceof Error ? errorParam
+				: new Error(String(errorParam));
+
+		try {
+			error.message = `${who}:> ${error.message}`;
+		}
+		catch {
+			console.error(`${who}:> ${error.message}`);
+		}
+		
 		throw error;
 	}
 }
