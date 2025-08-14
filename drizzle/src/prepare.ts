231e641fb586bf5e2a7ecbf38062e138ab073b0c
yair <yairopro@gmail.com>
2025-08-14 08:14:12 +0300

    drizzle prepare.single

--- drizzle/src/prepare.ts
+++ drizzle/src/prepare.ts
@@ -1,20 +1,40 @@
 import { toDictionary3 } from "@coffee-break/toolbox/object/toDictionary";
-import type { fn } from "@coffee-break/toolbox/type/fn";
 import { sql } from "drizzle-orm";
-import { identity } from "ramda";
+import { andThen, identity, nth, pipe } from "ramda";
 import type { PlaceholderDic, ValuesDic } from "./types";
 
-export default function prepare<const K extends string[], Q extends Query>(name: string, keys: K, build: BuildQuery<K, Q>) {
+// TODO move all params into config object
+export default function prepare<const K extends string[], Q extends Query>(name: string, keys: K, build: BuildQuery<K, Q>, config?: Config) {
 	const placholders = toDictionary3(keys, identity, key => sql.placeholder(key)) as
 		PlaceholderDic<K>;
 
-	const query = build(placholders).prepare(name);
-	return function execute(params: ValuesDic<K>) {
-		return query.execute(params) as queryOutput<Q>;
-	};
+	const listLimit = config?.limit ?? DEFAULT_LIMIT_LIST;
+	const listQuery = build(placholders).limit(listLimit).prepare(name);
+
+	type ListPromise = queryOutput<Q>;
+	function loadList(params: ValuesDic<K>) {
+		return listQuery.execute(params) as ListPromise;
+	}
+
+	const singleQuery = build(placholders).limit(1).prepare(name + '_single');
+	type List = Awaited<ListPromise>;
+	loadList.single = pipe(
+		(params: ValuesDic<K>) => singleQuery.execute(params) as ListPromise,
+		andThen(nth(0)<List>)
+	);
+
+	return loadList;
 }
 
+interface Config {
+	limit?: number
+}
 
-type Query = { prepare: fn<[string], { execute: fn<[any], Promise<any>> }> };
-type BuildQuery<K extends string[], Q extends Query> = (p: PlaceholderDic<K>) => Q;
-type queryOutput<Q extends Query> = ReturnType<ReturnType<Q['prepare']>['execute']>;
+export const DEFAULT_LIMIT_LIST = 100;
+
+export type Query = { limit(limit: number): Preparable };
+export type BuildQuery<K extends string[], Q extends Query> = (p: PlaceholderDic<K>) => Q;
+export type queryOutput<Q extends Query> = ReturnType<ReturnType<ReturnType<Q["limit"]>['prepare']>['execute']>;
+
+type Preparable = { prepare(name: string): Executable };
+type Executable = { execute(params: any): Promise<any> };
