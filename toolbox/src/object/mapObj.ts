4519f6837416ded4b05c3a0c959ac4e2b93ad8ff
yair <yairopro@gmail.com>
2025-10-01 10:52:30 +0300

    chore

--- toolbox/src/object/mapObj.ts
+++ toolbox/src/object/mapObj.ts
@@ -1,6 +1,6 @@
-import { map, type ValueOfUnion, } from "ramda";
+import { mapObjIndexed, type ValueOfUnion, } from "ramda";
 
-export const mapObj = map as {
+export const mapObj = mapObjIndexed as {
 	<A, B>(fn: (x: A) => B): <U extends Record<PropertyKey, A>>(dict: U) => Record<keyof U, B>;
 	<U extends object, B>(fn: (x: ValueOfUnion<U>) => B, dict: U): Record<keyof U, B>
 };
