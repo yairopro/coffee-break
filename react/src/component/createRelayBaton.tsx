a827358c745ce00f08582281cde079a18f66f235
yair <yairopro@gmail.com>
2026-01-08 07:56:40 +0200

    Baton.genScript(data)

--- react/src/component/createRelayBaton.tsx
+++ react/src/component/createRelayBaton.tsx
@@ -1,14 +1,19 @@
 import React from "react";
 
 export default function createRelayBaton<T>(name: string) {
-	function Baton({ data }: ClientDataProps<T>) {
+	function genScript(data: T) {
 		const serialized = JSON.stringify(data);
-		return <script>{`window.${GLOBAL_KEY} ??= {}; window["${GLOBAL_KEY}"]["${name}"] = ${serialized};`}</script>
+		return `globalThis["${GLOBAL_KEY}"] ??= {}; globalThis["${GLOBAL_KEY}"]["${name}"] = ${serialized};`
 	}
+
+	function Baton({ data }: ClientDataProps<T>) {
+		return <script>{genScript(data)}</script>
+	}
+	Baton.genScript = genScript;
 
 	function getBaton() {
 		// @ts-expect-error
-		return globalThis.window?.[GLOBAL_KEY]?.[name] as T | undefined;
+		return globalThis[GLOBAL_KEY]?.[name] as T | undefined;
 	}
 
 	return [Baton, getBaton] as const;
